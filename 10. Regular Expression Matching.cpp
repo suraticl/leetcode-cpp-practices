b2eeed048870674aa92a03ebd5cdad6eed0f14df
suraticl <114895520+suraticl@users.noreply.github.com>
2022-10-11 13:38:02 +0300

    up date 10.Regular Expression Matching.cpp

--- 10. Regular Expression Matching.cpp
+++ 10. Regular Expression Matching.cpp
@@ -223,3 +223,22 @@ public:
         return dp[0][0];
     }
 };
+// solution for problem leet code probleme 
+// 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0, i=0, j=height.size()-1;
+        
+        while(i<j)
+        {
+            int x = min(height[i], height[j])*(j-i);
+            ans = max(ans, x);
+            if(height[i]<height[j])
+            i++;
+            else 
+            j--;
+        }
+        return ans;
+    }
+};
